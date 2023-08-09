beaafb83edcac0bda29aa01e5a99488d75becca6
bishop-of-intensity <79968439+bishop-of-intensity@users.noreply.github.com>
2023-08-09 13:36:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 // SECTION-EXPANDER
 $('#youtube').click(function() {
     console.log('youtube link')
-    // var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
-    // win.focus();
+    var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
+    win.focus();
 });
 $('#facebook').click(function() {
     console.log('facebook link')
-    // var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
-    // win.focus();
+    var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
+    win.focus();
 });
 $('#instagram').click(function() {
     console.log('instagram link')
-    // var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
-    // win.focus();
+    var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
+    win.focus();
 });
 $('#twit').click(function() {
     console.log('twit link')
-    // var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
-    // win.focus();
+    var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
+    win.focus();
 });
 $('#tiktok').click(function() {
     console.log('tiktok link')
-    // var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
-    // win.focus();
+    var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
+    win.focus();
 });
 $('#spotify').click(function() {
     console.log('spotify link')
-    // var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
-    // win.focus();
+    var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
+    win.focus();
 });
 $('#tickets').click(function() {
     console.log('tickets link')
-    // var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
-    // win.focus();
+    var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
+    win.focus();
 });
 
 
@@ -54,6 +54,6 @@ $('#close-popup').click(function() {
 $('#album-1').click(function() {
     console.log('albums-1 link')
     $('.band_container-2').addClass('visible-popup')
-    // var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
-    // win.focus();
+    var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
+    win.focus();
 });
