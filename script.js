e5fe9a4e20800d8d9de09694ff23314bda320711
bishop-of-intensity <79968439+bishop-of-intensity@users.noreply.github.com>
2023-08-20 11:38:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -56,9 +56,72 @@ $('#close-popup').click(function() {
 
 
 
+
+
+
 $('#album-1').click(function() {
     console.log('albums-1 link')
     $('.band_container-2').addClass('visible-popup')
     var win = window.open('https://github.com/bishop-of-intensity/Holistic-Approach', '_blank');
     win.focus();
+});
+
+
+
+
+
+
+
+$('#close-popup-3').click(function() {
+    $('.band_container-3').removeClass('visible-popup');
+    $('#dude_1_txt').removeClass('visible-popup');
+    $('#dude_2_txt').removeClass('visible-popup');
+    $('#dude_3_txt').removeClass('visible-popup');
+    $('#dude_4_txt').removeClass('visible-popup');
+    $('#dude_5_txt').removeClass('visible-popup');
+    $('#dude_ALL_txt').removeClass('visible-popup');
+
+    $("#popup-member-img-overlay").removeClass();
+    $('#popup-member-img-overlay').addClass('popup-member-img-overlay');
+
+    $("#popup-member-img-portrait").removeClass();
+    $('#popup-member-img-portrait').addClass('popup-member-img-portrait');
+});
+
+
+$('#dude_1').click(function() {
+    $('.band_container-3').addClass('visible-popup');
+    $('#dude_1_txt').addClass('visible-popup');
+    $('#popup-member-img-overlay').addClass('popup-member-img-overlay-1');
+    $('#popup-member-img-portrait').addClass('popup-member-img-portrait-1');
+});
+$('#dude_2').click(function() {
+    $('.band_container-3').addClass('visible-popup');
+    $('#dude_2_txt').addClass('visible-popup');
+    $('#popup-member-img-overlay').addClass('popup-member-img-overlay-2');
+    $('#popup-member-img-portrait').addClass('popup-member-img-portrait-2');
+});
+$('#dude_3').click(function() {
+    $('.band_container-3').addClass('visible-popup');
+    $('#dude_3_txt').addClass('visible-popup');
+    $('#popup-member-img-overlay').addClass('popup-member-img-overlay-3');
+    $('#popup-member-img-portrait').addClass('popup-member-img-portrait-3');
+});
+$('#dude_4').click(function() {
+    $('.band_container-3').addClass('visible-popup');
+    $('#dude_4_txt').addClass('visible-popup');
+    $('#popup-member-img-overlay').addClass('popup-member-img-overlay-4');
+    $('#popup-member-img-portrait').addClass('popup-member-img-portrait-4');
+});
+$('#dude_5').click(function() {
+    $('.band_container-3').addClass('visible-popup');
+    $('#dude_5_txt').addClass('visible-popup');
+    $('#popup-member-img-overlay').addClass('popup-member-img-overlay-5');
+    $('#popup-member-img-portrait').addClass('popup-member-img-portrait-5');
+});
+$('#dude_ALL').click(function() {
+    $('.band_container-3').addClass('visible-popup');
+    $('#dude_ALL_txt').addClass('visible-popup');
+    $('#popup-member-img-overlay').addClass('popup-member-img-overlay-ALL');
+    $('#popup-member-img-portrait').addClass('popup-member-img-portrait-ALL');
 });
